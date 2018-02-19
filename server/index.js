e5e21a49d17a464b7f870fd491a57ad5df0055f3
Steven Isbell <steven.isbell18@gmail.com>
2018-02-18 22:04:41 -0600

    v1

---
+++ server/index.js
@@ -0,0 +1,111 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const csv = require("fast-csv");
+const nodemailer = require("nodemailer");
+
+const sendMail = require(`${__dirname}/nodemailer`);
+const { email, pass } = require(`${__dirname}/creds`);
+
+const sleep = function(timeToWait) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(true);
+    }, timeToWait);
+  });
+};
+
+// READ DATA FROM CSV
+
+const parseData = () => {
+  let csvData = [];
+  fs
+    .createReadStream(`${__dirname}/../dataToScrape.csv`)
+    .pipe(csv({ headers: true }))
+    .on("data", data => {
+      csvData.push(data);
+    })
+    .on("end", () => scrapeForJobs(csvData));
+};
+
+parseData();
+
+// WRITE DATA TO CSV
+
+const writeData = data => {
+  let dataToWrite = data.map(val => [
+    val.name,
+    val.url,
+    val.employer,
+    val.title
+  ]);
+  dataToWrite.unshift(["Name", "Url", "Employer", "Title"]);
+  const writeStream = fs.createWriteStream("./jobs.csv");
+  csv.write(data, { headers: true }).pipe(writeStream);
+  sendMail();
+};
+
+// SCRAPE FOR POSITION
+
+const scrapeForJobs = async urlData => {
+  const jobData = [];
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.goto("https://www.linkedin.com");
+    await sleep(3000);
+    await page.click("#login-email");
+    await page.keyboard.type(email);
+    await page.click("#login-password");
+    await page.keyboard.type(pass);
+    await page.click("#login-submit");
+    await page.waitForNavigation();
+
+    for (let i = 0; i < urlData.length; i++) {
+      await page.goto(urlData[i].url);
+
+      await sleep(2000);
+
+      await page.screenshot({
+        path: `${__dirname}/screenshots/example${i}.png`
+      });
+
+      const data = await page.evaluate(sel => {
+        const currentJobBlockHolder =
+          "#experience-section > ul.section-info > li.pv-profile-section__card-item > a > .pv-entity__summary-info" ||
+          null;
+        const currentJobBlock =
+          document.querySelector(currentJobBlockHolder) || null;
+        console.log("JOB BLOCK: ", currentJobBlock);
+        if (currentJobBlock) {
+          const title = document.querySelector(`${currentJobBlockHolder} > h3`)
+            .innerText;
+          const employer = document.querySelector(
+            `${currentJobBlockHolder} > h4 > .pv-entity__secondary-title`
+          ).innerText;
+          return {
+            title,
+            employer,
+            name: sel.name,
+            url: sel.url
+          };
+        } else
+          return {
+            title: null,
+            employer: null,
+            name: sel.name,
+            url: sel.url
+          };
+      }, urlData[i]);
+
+      jobData.push(data);
+    }
+
+    await browser.close();
+    writeData(jobData);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// scrapeForJobs();
---
+++ server/nodemailer.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+
+const { email, pass } = require("./creds");
+const mailList = require("./mailList");
+
+const smtpTransport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: email,
+    pass
+  }
+});
+
+const sendMail = () =>
+  smtpTransport.sendMail(
+    {
+      from: `Joe Smith ${email}`,
+      to: mailList,
+      subject: "Job Details You Requested",
+      text: `Here's the details you requested.`,
+      attachments: [{ filename: "jobs.csv", path: `${__dirname}/../jobs.csv` }]
+    },
+    function(error, response) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(response);
+      }
+      smtpTransport.close();
+    }
+  );
+
+module.exports = sendMail;
